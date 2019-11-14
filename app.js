888550de71f6d3ce15022a27e82c4e7d03f785dc
alicjakardynal <k.alicja66@gmail.com>
2019-11-14 15:56:41 +0100

    inputFixed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const input = document.querySelector('.input_personal');
     input.addEventListener('click', function() {
         const div = this.firstElementChild.firstElementChild;
-        if (div.style.display === "none") {
+        if (div.style.display !== "block") {
             div.style.display = "block"
         } else {
             div.style.display = "none"
