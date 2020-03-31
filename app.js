624c5272ec634e1aaecc141e1f27ce4ec7ff31d7
alicjakardynal <k.alicja66@gmail.com>
2020-03-31 14:32:35 +0200

    trackingCalculatorsSum

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-
   const input_in_form = document.querySelector(".input_personal");
   input_in_form.addEventListener("click", function() {
     const div = this.firstElementChild.firstElementChild;
@@ -10,33 +9,75 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
   const input_account = document.querySelector(".choose_option:first-of-type");
-  const account_pricing_space=document.querySelector(".account-space");
-  console.log(account_pricing_space)
+  const account_pricing_space = document.querySelector(".account-space");
+
   input_account.addEventListener("click", function() {
     const account = this.firstElementChild.nextElementSibling;
     if (account.style.display !== "block") {
       account.style.display = "block";
-      account_pricing_space.style.display="flex";
+      account_pricing_space.style.display = "flex";
     } else {
       account.style.display = "none";
-      account_pricing_space.style.display="none";
+      account_pricing_space.style.display = "none";
     }
   });
 
   const input_rental = document.querySelector(".choose_option:nth-of-type(2)");
-  const rental_pricing_space=document.querySelector(".rental-space");
-  
+  const rental_pricing_space = document.querySelector(".rental-space");
+
   input_rental.addEventListener("click", function() {
     const rental = this.firstElementChild.nextElementSibling;
     if (rental.style.display !== "block") {
       rental.style.display = "block";
-      rental_pricing_space.style.display="flex";
+      rental_pricing_space.style.display = "flex";
     } else {
       rental.style.display = "none";
-      rental_pricing_space.style.display="none";
+      rental_pricing_space.style.display = "none";
     }
   });
 
+  function sum() {}
 
+  const quantity = document.querySelector(
+    ".inputs_section input:first-of-type"
+  );
+  quantity.addEventListener("input", function() {
+    const quantityInput = quantity.value;
+    const products = document.querySelector(".products");
+    products.innerHTML = quantityInput;
+    if (quantity.value == "") {
+      products.innerHTML = "0";
+    }
+    const productsSum = document.querySelector(
+      ".summary-space:first-of-type .price span"
+    );
+    const productsNumber = parseInt(productsSum.innerText);
+    const newProductsSum = 0.5 * quantityInput;
+    productsSum.innerHTML = newProductsSum;
+  });
+
+
+  const orders = document.querySelector(".inputs_section input:nth-of-type(2)");
+  orders.addEventListener("input", function() {
+    const ordersInput = orders.value;
+    const monthly = document.querySelector(".orders");
+    monthly.innerHTML = ordersInput;
+    if (orders.value == "") {
+      monthly.innerHTML = "0";
+    }
+    const ordersSum = document.querySelector(
+      ".summary-space:nth-of-type(2) .price span"
+    );
+    const ordersNumber = parseInt(ordersSum.innerText);
+    const newOrdersSum = 0.5 * ordersInput;
+    ordersSum.innerHTML = newOrdersSum;
+
+
+  });
+
+  const packageSum = document.querySelector(
+    ".summary-space:nth-of-type(3) .price span"
+  );
+  console.log(packageSum)
 
 });
