954a271075b54826e59c52ea2ce99531c840c399
alicjakardynal <k.alicja66@gmail.com>
2020-04-01 14:27:18 +0200

    finishedCalculator

--- app.js
+++ app.js
@@ -10,33 +10,75 @@ document.addEventListener("DOMContentLoaded", function() {
   });
   const input_account = document.querySelector(".choose_option:first-of-type");
   const account_pricing_space = document.querySelector(".account-space");
+  const account_price=document.querySelector(".account-space span");
+  
 
   input_account.addEventListener("click", function() {
     const account = this.firstElementChild.nextElementSibling;
     if (account.style.display !== "block") {
       account.style.display = "block";
       account_pricing_space.style.display = "flex";
+      account_price.innerText="35";
+      const new__account_price=parseInt(account_price.innerText);
+      
+      
     } else {
       account.style.display = "none";
       account_pricing_space.style.display = "none";
+      account_price.innerText="0";
+      new__account_price=parseInt(account_price.innerText);
+      
+      
+
     }
+    sum();
   });
 
   const input_rental = document.querySelector(".choose_option:nth-of-type(2)");
   const rental_pricing_space = document.querySelector(".rental-space");
+  const rental_price=document.querySelector(".rental-space span");
 
   input_rental.addEventListener("click", function() {
     const rental = this.firstElementChild.nextElementSibling;
     if (rental.style.display !== "block") {
       rental.style.display = "block";
       rental_pricing_space.style.display = "flex";
+      rental_price.innerText="5"
+      const new_rental_price=parseInt(rental_price.innerText);
+      
+      
     } else {
       rental.style.display = "none";
       rental_pricing_space.style.display = "none";
+      rental_price.innerText="0";
+      new_rental_price=parseInt(rental_price.innerText);
+    
+      
+
     }
+    sum();
   });
 
-  function sum() {}
+  const total_price=document.querySelector(".total span");
+  const all_prices=document.querySelectorAll(".price span");
+  
+
+  
+  function sum() {
+    let sumary=0;
+    
+    for (let i=0;i<all_prices.length;i++){
+     let rand = all_prices[i];
+     const numbers = parseFloat(rand.innerText,4);
+     sumary+= numbers;
+    }
+    
+    total_price.innerText=sumary;
+
+    
+
+
+  }
 
   const quantity = document.querySelector(
     ".inputs_section input:first-of-type"
@@ -53,9 +95,10 @@ document.addEventListener("DOMContentLoaded", function() {
     );
     const productsNumber = parseInt(productsSum.innerText);
     const newProductsSum = 0.5 * quantityInput;
-    productsSum.innerHTML = newProductsSum;
-  });
+    productsSum.innerText = newProductsSum;
 
+    sum();
+  });
 
   const orders = document.querySelector(".inputs_section input:nth-of-type(2)");
   orders.addEventListener("input", function() {
@@ -71,8 +114,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const ordersNumber = parseInt(ordersSum.innerText);
     const newOrdersSum = 0.25 * ordersInput;
     ordersSum.innerHTML = newOrdersSum;
+    sum();
   });
-
 
   const packageSelect = document.querySelector("#package");
   console.log(packageSelect);
@@ -84,24 +127,25 @@ document.addEventListener("DOMContentLoaded", function() {
     const packageSum = document.querySelector(
       ".summary-space:nth-of-type(3) .price span"
     );
-let newPackageSum=0;
+    let newPackageSum = 0;
     switch (this.value) {
       case "basic":
         packageName.innerHTML = "Basic";
         packageSum.innerText = "0";
-        newPackageSum= parseInt(packageSum.innerText);
+        newPackageSum = parseInt(packageSum.innerText);
         break;
       case "professional":
-          packageName.innerHTML = "Professional";
-          packageSum.innerText = "25";
-          newPackageSum= parseInt(packageSum.innerText);
-          break;
+        packageName.innerHTML = "Professional";
+        packageSum.innerText = "25";
+        newPackageSum = parseInt(packageSum.innerText);
+        break;
       case "premium":
-          packageName.innerHTML = "Premium";
-          packageSum.innerText = "60";
-          newPackageSum= parseInt(packageSum.innerText);
-          break;
-
+        packageName.innerHTML = "Premium";
+        packageSum.innerText = "60";
+        newPackageSum = parseInt(packageSum.innerText);
+        break;
     }
+sum();
+
   });
 });
